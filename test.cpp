ff28987d42735089bd9b5bbe8ba4d04900dd240a
wai157 <hoang150703@gmail.com>
2022-12-03 22:46:34 +0700

    v2

--- test.cpp
+++ test.cpp
@@ -2,31 +2,6 @@
 #include "pbl2.cpp"
 using namespace std;
 
-class base{
-    public:
-        virtual void foo(){
-            cout<<"base nothing";
-        }
-        virtual void foo(int x=0){
-            cout<<"base int";
-        }
-};
-
-class derive: public base{
-    public:
-        virtual void foo(){
-            cout<<"derive nothing";
-        }
-        virtual void foo(int x=0){
-            cout<<"derive int";
-        }
-};
-
 int main(){
-    base* ptr;
-    derive obj;
-    ptr = &obj;
-    ptr->foo();
-    ptr->foo(5);
-    obj.foo(5);
+    
 }
